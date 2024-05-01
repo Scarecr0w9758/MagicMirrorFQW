a9b40289daaa3f2ca5c635e192c2861cf165b1d1
demin.m <Matway.demin@yandex.ru>
2024-05-01 19:52:42 +0500

    feat: add Main grid component to display grid on page

---
+++ src/renderer/src/widgets/MainGridPage.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import { h, reactive, ref, defineAsyncComponent, computed, onMounted } from 'vue'
+import { GridItem, GridLayout } from 'vue3-drr-grid-layout'
+
+// const gridItemComponent = computed(() => VueGridLayout.GridItem)
+
+// let GridLayout = VueGridLayout.GridLayout
+// let GridItem = VueGridLayout.GridItem
+// console.log(GridLayout)
+// const gridLayoutComponent = computed(() => VueGridLayout.GridLayout)
+const layout = ref([
+  { x: 0, y: 0, w: 2, h: 2, i: '0' },
+  { x: 2, y: 2, w: 2, h: 2, i: '1' },
+  { x: 4, y: 4, w: 2, h: 3, i: '2' },
+  { x: 6, y: 6, w: 2, h: 3, i: '3' },
+  { x: 8, y: 8, w: 2, h: 3, i: '4' }
+])
+</script>
+
+<template>
+  <div class="layout">
+    <GridLayout
+      class="dashboard"
+      v-model:layout="layout"
+      :col-num="6"
+      :row-height="60"
+      :is-mirrored="false"
+      :vertical-compact="true"
+      :margin="[10, 10]"
+      :use-css-transforms="true"
+    >
+      <template #default="{ gridItemProps }">
+        <GridItem
+          v-for="item in layout"
+          :x="item.x"
+          v-bind="gridItemProps"
+          :y="item.y"
+          :w="item.w"
+          :h="item.h"
+          :i="item.i"
+          class="text"
+          :key="item.i"
+        >
+          {{ item.i }}
+        </GridItem>
+      </template>
+    </GridLayout>
+  </div>
+</template>
+<style scoped lang="scss">
+.layout {
+  width: 100%;
+}
+.text {
+  padding: 0.5rem;
+  background: linear-gradient(315deg, #42d392 25%, #647eff);
+}
+.dashboard {
+  width: calc(100% + 60px);
+  margin: -20px -30px 0 -30px;
+
+  &__header {
+    margin: 0 -20px;
+    padding: 10px 10px;
+    align-items: center;
+
+    &__sticky {
+      position: sticky;
+      top: 65px;
+      z-index: 3;
+      background: rgba(246, 243, 243, 0.45);
+      box-shadow: 0px 2px 10px 2px rgba(34, 60, 80, 0.2);
+      border-radius: 10px;
+    }
+  }
+
+  @media (max-width: 992px) {
+    width: calc(100% + 30px);
+    margin: -20px 0 0 -20px;
+
+    &__header {
+      margin: 0;
+    }
+  }
+}
+</style>
