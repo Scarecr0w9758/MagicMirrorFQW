8ee76ae37e5cbab6ed2fd693bfbacb005f95c0b8
demin.m <Matway.demin@yandex.ru>
2024-05-25 09:13:26 +0500

    feat: add baseInput to easily manipulate entities

---
+++ src/renderer/src/shared/forms/BaseInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-input :show-password="type == 'password'" :type="type" v-model="modelValue" show-word-limit />
+</template>
+
+<script setup lang="ts">
+import { useVModel } from "@vueuse/core"
+
+const props = withDefaults(
+  defineProps<{
+    text: string
+    type?: string
+  }>(),
+  {}
+)
+const emits = defineEmits(["update:modelValue"])
+const modelValue = useVModel(props, "text", emits)
+</script>
