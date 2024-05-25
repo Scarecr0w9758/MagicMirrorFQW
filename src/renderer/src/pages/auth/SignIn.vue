3b432cba438a5b54dca5737c8132f668a712fac6
demin.m <Matway.demin@yandex.ru>
2024-05-25 09:12:46 +0500

    feat: add SignIn

---
+++ src/renderer/src/pages/auth/SignIn.vue
@@ -0,0 +1,14 @@
+<template>
+  <base-input v-model:text="login"></base-input>
+  <input label="email" type="text" show-word-limit :maxlength="10" />
+  <input label="password" type="text" />
+  <input type="checkbox" />
+  <el-button>Войти</el-button>
+</template>
+
+<script setup lang="ts">
+import BaseInput from "@/renderer/src/shared/forms/BaseInput.vue"
+import { ref } from "vue"
+
+const login = ref()
+</script>
