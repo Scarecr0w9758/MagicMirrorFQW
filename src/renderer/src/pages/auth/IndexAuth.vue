c1442034e6d52b075f41f4efb6bda968e04c4aca
demin.m <Matway.demin@yandex.ru>
2024-05-25 09:12:22 +0500

    feat: add indexAuth page to control registration and signInform
    *transistion will be included to change between forms

---
+++ src/renderer/src/pages/auth/IndexAuth.vue
@@ -0,0 +1,22 @@
+<template>
+  <br />
+  MAIN AUTH!
+  <div class="links">
+    <router-link :to="routerKeys.AUTH.children.LOGIN.path">Sign in</router-link>
+    <router-link :to="routerKeys.AUTH.children.REGISTRATION.path">Registration</router-link>
+  </div>
+
+  <router-view> </router-view>
+</template>
+
+<script setup lang="ts">
+import { routerKeys } from "@/shared/router/router-keys"
+console.log("ath!")
+</script>
+<style scoped>
+.links {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+}
+</style>
