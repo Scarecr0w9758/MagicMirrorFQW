debf85937f66c4effa0acd4bbc9756d446e07166
demin.m <Matway.demin@yandex.ru>
2024-05-25 09:15:56 +0500

    feat: add router-keys dictionary

---
+++ src/shared/router/router-keys.ts
@@ -0,0 +1,35 @@
+export const routerKeys = {
+  HOME: {
+    path: "/",
+    name: "home"
+  },
+  MAIN: {
+    path: "/main",
+    name: "main"
+  },
+  AUTH: {
+    path: "/auth",
+    name: "auth",
+    children: {
+      REGISTRATION: {
+        path: "/auth/registration",
+        name: "auth-registration"
+        // getPath: (pageId) => `/about-page/${pageId}`
+      },
+      LOGIN: {
+        path: "/auth/login",
+        name: "auth-login"
+      }
+      // DETAIL_CREATE: {
+      //   path: "/about-page/create-new",
+      //   name: "about-page-create-new"
+      //   // getPath: (pageId) => `/about-page/${pageId}/edit`,
+      // },
+      // DETAIL_EDIT: {
+      //   path: "/about-page/:pageId/edit",
+      //   name: "about-page-detail-edit",
+      //   getPath: (pageId) => `/about-page/${pageId}/edit`
+      // }
+    }
+  }
+}
