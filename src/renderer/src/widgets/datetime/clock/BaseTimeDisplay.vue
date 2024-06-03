f6f817ef4b4279eb86d5af1df638129168d9f80e
demin.m <Matway.demin@yandex.ru>
2024-06-03 09:52:47 +0500

    feat: add baseTimeDisplay component

---
+++ src/renderer/src/widgets/datetime/clock/BaseTimeDisplay.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref, onUnmounted } from "vue"
+import dayjs from "dayjs"
+
+const props = withDefaults(
+  defineProps<{
+    backgroundColor: string | number
+    timeFormat: "24-hour" | "12-hour"
+    isLoading?: boolean
+  }>(),
+  {
+    backgroundColor: "biege",
+    timeFormat: "24-hour",
+    isLoading: false
+  }
+)
+
+const currentTime = ref(dayjs().format("HH:mm:ss"))
+const currentHours = ref(dayjs().format(props.timeFormat == "24-hour" ? "HH" : "hh"))
+const currentMinutes = ref(dayjs().format("mm"))
+const currentSeconds = ref(dayjs().format("ss"))
+const currentTimeFormat = ref(dayjs().format("a"))
+let timeInterval = setInterval(() => {
+  currentTime.value = dayjs().format("HH:mm:ss")
+  currentHours.value = dayjs().format(props.timeFormat == "24-hour" ? "HH" : "hh")
+  currentMinutes.value = dayjs().format("mm")
+  currentSeconds.value = dayjs().format("ss")
+}, 1000)
+onUnmounted(() => clearInterval(timeInterval))
+</script>
+
+<template>
+  <!-- {{ currentTime }} -->
+  <div class="time-block">
+    <div>
+      {{ currentHours }}
+    </div>
+    <div>
+      {{ currentMinutes }}
+    </div>
+    <div>
+      {{ currentTimeFormat }}
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+.time-block {
+  display: flex;
+  gap: 1rem;
+  font-size: 3rem;
+  background-color: rgb(28, 28, 28);
+}
+</style>
