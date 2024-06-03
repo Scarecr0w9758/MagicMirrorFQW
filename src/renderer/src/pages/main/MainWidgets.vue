3c4f70e272a5ab5070464e5952987b8ec41d21fe
demin.m <Matway.demin@yandex.ru>
2024-06-03 09:50:58 +0500

    feat: create base-widgets component

---
+++ src/renderer/src/pages/main/MainWidgets.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+// import { routerKeys } from "../../../src/shared/router/router-keys"
+import { routerKeys } from "@/shared/router/router-keys"
+// import BaseClocks from "../../widgets/datetime/clock/BaseClocks.vue"
+import { BaseCalendar, BaseClocks, BaseTimeDisplay } from "../../widgets/datetime"
+
+const ipcHandle = () => window.electron.ipcRenderer.send("ping")
+</script>
+
+<template>
+  <base-calendar></base-calendar>
+  <base-time-display time-format="12-hour"></base-time-display>
+</template>
+<style scoped>
+.text {
+  background-color: darkslategray;
+  border-radius: 5px;
+  padding: 1rem;
+}
+</style>
