a5ecf19f9e61cdb73f1e54289af41dfe4f8bd5d6
demin.m <Matway.demin@yandex.ru>
2024-06-05 00:21:19 +0500

    feat(login): add nice button to go to registrate page

--- src/renderer/src/pages/auth/SignIn.vue
+++ src/renderer/src/pages/auth/SignIn.vue
@@ -14,23 +14,37 @@
       <el-form-item>
         <el-button
           :loading="isFetchLoading"
-          color="#626aef"
-          class="register-block__submit"
+          class="register-block__submit primary-gradient-button"
           @click="onSubmit"
         >
           Войти в аккаунт
         </el-button>
+
+        <!-- <base-button :is-loading="true" class="register-block__submit">
+          Войти в аккаунт</base-button
+          > -->
       </el-form-item>
+      <el-button
+        color="warning"
+        :loading="isFetchLoading"
+        class="register-block__submit success-hover"
+        @click="router.push(routerKeys.AUTH.children.REGISTRATION.path)"
+      >
+        Регистрация
+      </el-button>
     </el-form>
   </div>
 </template>
 
 <script setup lang="ts">
 import BaseInput from "@/renderer/src/shared/forms/BaseInput.vue"
+import { BaseButton } from "@/renderer/src/shared/forms"
 import { registerUser } from "@/shared/api/requests/user/user"
 import { addGlobalError } from "@/shared/helpers/errors/errors-actions"
 import { ERRORS_LIST } from "@/shared/symbols/errors"
 import { inject, ref } from "vue"
+import { useRouter } from "vue-router"
+import { routerKeys } from "@/shared/router/router-keys"
 
 const isFetchLoading = ref(false)
 const login = ref()
@@ -38,6 +52,8 @@ const email = ref()
 const name = ref()
 const password = ref()
 const injectedError = inject(ERRORS_LIST)!
+
+const router = useRouter()
 console.log("injErr:", injectedError)
 // const { addError } = inject(ERRORS_LIST)
 
