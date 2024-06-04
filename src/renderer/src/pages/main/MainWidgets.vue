a561ef182b6097fd554cce8be94484c67eb8e6b3
demin.m <Matway.demin@yandex.ru>
2024-06-05 00:19:10 +0500

    feat(widgetsList): add grid layout
    modify main widgets list

--- src/renderer/src/pages/main/MainWidgets.vue
+++ src/renderer/src/pages/main/MainWidgets.vue
@@ -2,19 +2,50 @@
 // import { routerKeys } from "../../../src/shared/router/router-keys"
 import { routerKeys } from "@/shared/router/router-keys"
 // import BaseClocks from "../../widgets/datetime/clock/BaseClocks.vue"
-import { BaseCalendar, BaseClocks, BaseTimeDisplay } from "../../widgets/datetime"
+import { BaseCalendar, BaseClocks, BaseTimeDisplay, BaseDateDisplay } from "../../widgets/datetime"
 
 const ipcHandle = () => window.electron.ipcRenderer.send("ping")
 </script>
 
 <template>
-  <base-calendar></base-calendar>
-  <base-time-display time-format="12-hour"></base-time-display>
+  <div class="widgets-grid">
+    <div class="widgets-grid__leftTop">
+      <base-time-display time-format="12-hour"></base-time-display>
+      <base-date-display time-format="24-hour"></base-date-display>
+    </div>
+    <base-calendar class="widgets-grid__centerTop"></base-calendar>
+    <span class="widgets-grid__rightTop">'hi'</span>
+    <span class="widgets-grid__left">'hi'</span>
+    <span class="widgets-grid__center">'hi'</span>
+    <span class="widgets-grid__right">'hi'</span>
+  </div>
 </template>
-<style scoped>
-.text {
-  background-color: darkslategray;
-  border-radius: 5px;
-  padding: 1rem;
+<style scoped lang="scss">
+.widgets-grid {
+  display: grid;
+  grid-template-areas:
+    "leftTop centerTop rightTop"
+    "left center right"
+    "leftBotton centerBottom rightBottom";
+  grid-template-columns: 1fr 3fr 1fr;
+
+  &__leftTop {
+    grid-area: leftTop;
+  }
+  &__centerTop {
+    grid-area: centerTop;
+  }
+  &__rightTop {
+    grid-area: rightTop;
+  }
+  &__left {
+    grid-area: left;
+  }
+  &__center {
+    grid-area: center;
+  }
+  &__right {
+    grid-area: right;
+  }
 }
 </style>
